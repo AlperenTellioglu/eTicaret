6c99ba9074f570e98ed334bc2800fb2ee403a141
muhammettalhakaymak23214 <talha.kaymak@bil.omu.edu.tr>
2024-06-19 20:27:14 +0300

    RegisterServlet güncellendi.

--- src/main/java/eTicaret/RegisterServlet.java
+++ src/main/java/eTicaret/RegisterServlet.java
@@ -19,62 +19,71 @@ public class RegisterServlet extends HttpServlet {
 
 	private static final long serialVersionUID = -4722228995118068512L;
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.getRequestDispatcher("register.jsp").forward(req, resp);
-	}
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("register.jsp").forward(req, resp);
+    }
 
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String email = req.getParameter("email");
-		String ad = req.getParameter("ad");
-		String soyad = req.getParameter("soyad");
-		String password = req.getParameter("password");
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String email = req.getParameter("email");
+        String ad = req.getParameter("ad");
+        String soyad = req.getParameter("soyad");
+        String password = req.getParameter("password");
 
-		Connection conn = null;
+        Connection conn = null;
 
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			conn = DatabaseConfiguration.getConnection();
-			String sql = "INSERT INTO kullanicilar (ad, soyad, email, password) VALUES (?, ?, ?, ?)";
-			PreparedStatement statement = conn.prepareStatement(sql);
-			statement.setString(1, ad);
-			statement.setString(2, soyad);
-			statement.setString(3, email);
-			statement.setString(4, password);
+        try {
+            conn = DatabaseConfiguration.getConnection();
+            String sql = "INSERT INTO kullanicilar (ad, soyad, email, password) VALUES (?, ?, ?, ?)";
+            PreparedStatement statement = conn.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+            statement.setString(1, ad);
+            statement.setString(2, soyad);
+            statement.setString(3, email);
+            statement.setString(4, password);
 
-			int result = statement.executeUpdate();
-			conn.close();
-			 ResultSet resultSet = statement.executeQuery();
-			 int kullaniciId = resultSet.getInt("kullanici_id");
-			
-			if (result > 0) {
-				System.out.println("basarili kayıt");
-				
-				if(email.toLowerCase().startsWith("admin@")) {
-					resp.sendRedirect("admin/dashboard");
-					
-                    req.getSession().setAttribute("isAdmin", true);
-                    req.getSession().setAttribute("kullaniciId", kullaniciId);
-                    req.getSession().setAttribute("kullaniciAdi", ad +" "+ soyad);
-				}
-				else {
-					resp.sendRedirect("index.jsp");
-					
-                    req.getSession().setAttribute("isAdmin", false);
-                    req.getSession().setAttribute("kullaniciId", kullaniciId);
-                    req.getSession().setAttribute("kullaniciAdi", ad +" "+ soyad);
-					
-				}
-				
-				
-			} else {
-				System.out.println("basarisiz kayıt");
-				resp.sendRedirect("register");
-			}
-		} catch (ClassNotFoundException | SQLException e) {
-			e.printStackTrace();
-		}
-	}
+            int result = statement.executeUpdate();
+
+            if (result > 0) {
+                System.out.println("başarılı kayıt");
+
+                try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+                    if (generatedKeys.next()) {
+                        int kullaniciId = generatedKeys.getInt(1);
+
+                        if (email.toLowerCase().startsWith("admin@")) {
+                            
+                        	req.getSession().setAttribute("isAdmin", true);
+                        	req.getSession().setAttribute("isLoggedIn", true);
+                            req.getSession().setAttribute("kullaniciId", kullaniciId);
+                            req.getSession().setAttribute("kullaniciAdi", ad + " " + soyad);
+                        	
+                        	resp.sendRedirect("admin/dashboard");
+
+                            
+                        } else {
+                            
+                        	req.getSession().setAttribute("isAdmin", false);
+                        	req.getSession().setAttribute("isLoggedIn", true);
+                            req.getSession().setAttribute("kullaniciId", kullaniciId);
+                            req.getSession().setAttribute("kullaniciAdi", ad + " " + soyad);
+                        	
+                            resp.sendRedirect("index.jsp");
+  
+                        }
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.println("başarısız kayıt");
+                resp.sendRedirect("register");
+            }
+
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
