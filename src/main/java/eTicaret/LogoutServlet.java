4b93f6ae8c93ca876ab66fc8bebac4393ddd0903
muhammettalhakaymak23214 <talha.kaymak@bil.omu.edu.tr>
2024-06-19 18:34:25 +0300

    LogoutServlet değiştirildi.

--- src/main/java/eTicaret/LogoutServlet.java
+++ src/main/java/eTicaret/LogoutServlet.java
@@ -8,11 +8,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/LoginOut")
-public class LoginOut extends HttpServlet {
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    public LoginOut() {
+    public LogoutServlet() {
         super();
     }
 
